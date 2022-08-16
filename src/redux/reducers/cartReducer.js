ee001bf7f48b4ef867cfe84947552b797f326065
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-16 17:12:30 +0600

    reducer updated

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,24 +1,61 @@
 import { DECREMENT, INCREMENT } from "../constants/cartConstants";
 
 const initialState = {
-  value: 0,
+  asus: {
+    qty: 20,
+    price: 35500,
+  },
+  dell: {
+    qty: 35,
+    price: 9300,
+  },
+  canon: {
+    qty: 72,
+    price: 36500,
+  },
+  cart: [],
 };
 
 const cartReducer = (state = initialState, action) => {
-  const { type, value, payload } = action;
+  const { type, payload } = action;
+
   switch (type) {
     case INCREMENT:
-      return {
+      var id = payload.id || "";
+      const newState = {
         ...state,
-        value: state.value + 1,
+        [id]: {
+          ...state[id],
+          qty: state[id].qty + 1,
+        },
+        cart: [...state.cart, id],
       };
+      return newState;
     case DECREMENT:
-      return {
-        ...state,
-        value: state.value - 1,
+      var id = payload.id || "";
+      const removeItem = (arr, item) => {
+        const newArr = [...arr];
+        const index = newArr.findIndex((element) => element === item);
+        if (index !== -1) {
+          newArr.splice(index, 1);
+          return newArr;
+        } else {
+          return newArr;
+        }
       };
+      const newUpdatedState = {
+        ...state,
+        [id]: {
+          ...state[id],
+          qty: state[id].qty - 1,
+        },
+        cart: [...state.cart.splice(1,1)],
+      };
+      return newUpdatedState;
 
     default:
       return state;
   }
 };
+
+export default cartReducer;
