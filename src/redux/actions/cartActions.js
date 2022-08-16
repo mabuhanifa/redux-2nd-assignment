24b2cc55f153f6a3b7aa230aa5b914e09ac878f6
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-16 11:42:15 +0600

    store added

---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,12 @@
+import { DECREMENT, INCREMENT } from "../constants/cartConstants";
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
