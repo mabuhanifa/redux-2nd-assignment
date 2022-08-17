928961afc33db4b2d128b8900c4a10926dcc3bf5
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-17 10:28:00 +0600

    middlware updated

--- src/redux/middleware/logger.js
+++ src/redux/middleware/logger.js
@@ -4,8 +4,11 @@ const logger = (store) => (next) => (action) => {
   const { qty: dellQty } = state.dell;
   const { qty: canonQty } = state.canon;
 
-  if ((0 < asusQty < 20) & (0 < dellQty < 35) & (0 < canonQty < 72)) {
+  if ((asusQty < 20) & (dellQty < 35) & (canonQty < 72)) {
     return next(action);
+  } else {
+    alert("You can not order more than available quantities");
+    window.location.reload();
   }
 };
 
