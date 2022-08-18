d6d767242b1e9d281347094ead80633c8669f8c4
shourovahmd <moh.abuhanifa@gmail.com>
2022-08-18 21:12:21 +0600

    logger updated

--- src/redux/middleware/logger.js
+++ src/redux/middleware/logger.js
@@ -1,29 +1,13 @@
-
 const logger = (store) => (next) => (action) => {
   const state = store.getState();
   const { qty: asusQty } = state.asus;
   const { qty: dellQty } = state.dell;
   const { qty: canonQty } = state.canon;
-  //   if ((asusQty < 20) & (dellQty < 35) & (canonQty < 72)) {
-  //     return next(action);
-  //   } else {
-  //     alert("You can not order more than available quantities");
-  //     window.location.reload();
-  //   }
-  // if ((asusQty < 20) & ( dellQty < 35) & ( canonQty < 72)) {
-  //   return next(action);
-  // }
-  // if (asusQty >= 20) {
-  //   alert("Can not order more than available quantity");
-  //   return next({
-  //     type: "error",
-  //     payload: {
-  //       value: 0,
-  //       id: "asus",
-  //     },
-  //   });
-  // }
-console.log(action);
+
+  if ((asusQty < 20) & (dellQty < 35) & (canonQty < 72)) {
+    return next(action);
+  }
+
   next(action);
 };
 
